fb25d70c2256e8c7696ec6934e4a01040e14a7e7
Sander Smits <sander.smits@nelen-schuurmans.nl>
2013-05-16 10:40:00 +0200

    Add MessageString model.

--- dbtranslator/models.py
+++ dbtranslator/models.py
@@ -3,7 +3,23 @@
 from __future__ import unicode_literals
 from __future__ import print_function
 
-# from django.db import models
+from django.db import models
 # from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class MessageString(models.Model):
+    """
+    Message translation for the given language.
+    
+    """ 
+    digest = models.CharField(max_length=40, db_index=True)
+    language = models.CharField(max_length=5, db_index=True)
+    message_id = models.TextField()
+    message_str = models.TextField(blank=True)
+
+    class Meta:
+        unique_together = ('hash', 'language')
+
+    def __unicode__(self):
+        return '%s: %s (%s)' % (self.message_id, self.message_str, self.language)
+
